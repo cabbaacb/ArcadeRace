778a42c0db4b9f87510e8f1a1b67db60588ca8e2
cabbaacb <103894445+cabbaacb@users.noreply.github.com>
2022-09-28 18:38:12 +0300

    created bot scripts

--- Assets/Scripts/BotInputController.cs
+++ Assets/Scripts/BotInputController.cs
@@ -6,6 +6,29 @@ namespace Cars
 {
     public class BotInputController : MonoBehaviour
     {
-        
+        private int _index = 0;
+
+        [SerializeField]
+        private BotTargetPoint[] _targetPoints;
+
+        private float GetAngle()
+        {
+            var target = _targetPoints[_index].transform.position;
+            target.y = transform.position.y;
+
+            var direction = target - transform.position;
+            return Vector3.SignedAngle(direction, transform.forward, transform.up);
+        }
+
+        private void FixedUpdate()
+        {
+            var targetPoint = _targetPoints[_index].transform.position;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<BotTargetPoint>() != null)
+                _index++;
+        }
     }
 }
